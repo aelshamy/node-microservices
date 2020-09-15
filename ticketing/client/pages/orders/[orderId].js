37f390d0b3de4ecd421675a9c305c495e6aed06a
Amr Elshamy <ammohamed@icct.com>
2020-09-15 15:00:49 -0400

    adding orders listing

--- ticketing/client/pages/orders/[orderId].js
+++ ticketing/client/pages/orders/[orderId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import React, { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from '../../hooks/use-request';
@@ -11,7 +12,7 @@ const OrderShow = ({ order, currentUser }) => {
     data: {
       orderId: order.id,
     },
-    onSuccess: (payment) => console.log(payment),
+    onSuccess: () => Router.push('/orders'),
   });
 
   useEffect(() => {
